4859dcc975a16969c4decb53a88e6e190281051d
Todd <toddrmyers11@gmail>
2021-09-09 08:29:12 -0400

    Add calculations to recipe ingredients Picker added

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -18,4 +18,65 @@ class RecipeModel: ObservableObject {
         
         
     }
+    
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            
+            //Get a single serving size by multiplying denominator by the recipe servings
+            denominator = denominator * recipeServings
+            // or denominator *= recipeServings
+            //Get target portion by multiplying numerator by target servings
+            numerator = numerator * targetServings
+            // or numerator *= targetServings
+            //Reduce fraction by greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            //Get the whole portion if the numerator is greater than the denominator
+            if numerator >= denominator {
+                //Calculate the whole portion
+                wholePortions = numerator/denominator
+                //Calculate the remainder
+                numerator = numerator % denominator
+                //Assign to portion String
+                portion += String(wholePortions)
+            }
+            //Express the remainder as a fraction
+            if numerator > 0 {
+                //Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " ": "" //shorthand if statement
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+            
+            
+            //if we need to pluralize
+            if wholePortions > 1 {
+                // Calculate the appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "            //inline if statement above
+            return portion + unit
+        }
+        
+        return portion
+    }
 }
