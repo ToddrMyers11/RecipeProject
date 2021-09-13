45033189d3c328ec3d0d90872c7b357e1b90d3d7
Todd <toddrmyers11@gmail>
2021-09-13 14:08:22 -0400

    Change to uniform font

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -11,19 +11,20 @@ struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
     @State var isDetailViewShowing = false
+    @State var tabSelectionIndex = 0
     var body: some View {
         
-        VStack (alignment: .leading) {
+        VStack (alignment: .leading, spacing: 0) {
            
         Text("Featured Recipes")
             .bold()
             .padding(.leading)
             .padding(.top, 40)
-            .font(.largeTitle)
+            .font(Font.custom("Avenir Heavy", size: 24))
             
             
         GeometryReader { geo in
-        TabView{
+            TabView (selection: $tabSelectionIndex) {
             //Loop through each recipe
             ForEach (0..<model.recipes.count)
             {index in
@@ -47,13 +48,14 @@ struct RecipeFeaturedView: View {
                                 .clipped()
                             Text(model.recipes[index].name)
                                 .padding(5)
-                            
+                                .font(Font.custom("Avenir", size: 15))
                                   
                                     }
                         
                     }
                     
                 })
+                .tag(index)
                 .sheet(isPresented: $isDetailViewShowing) {
                    // Show the recipe detail view
                     RecipeDetailView(recipe: model.recipes[index])
@@ -76,15 +78,29 @@ struct RecipeFeaturedView: View {
     }
         
             VStack (alignment: .leading, spacing: 10) {
-            Text("Preparation Time:")
-                .font(.headline)
-            Text("1 hour")
+                
+                Text("Preparation Time:")
+                .font(Font.custom("Avenir Heavy", size: 16))
+                Text(model.recipes[tabSelectionIndex].prepTime)
             Text("Highlights")
-                .font(.headline)
-            Text("Healthy, Hearty")
+                .font(Font.custom("Avenir Heavy", size: 16))
+                RecipeHighlights(highlights:
+                                    model.recipes[tabSelectionIndex].highlights)
+                
+         
         }
             .padding([.leading, .bottom])
         }
+        .onAppear(perform: {
+            setFeaturedIndex()
+        })
+    }
+        func setFeaturedIndex() {
+        // Find index of the first recipe that is featured
+           var index = model.recipes.firstIndex {(recipe) -> Bool in
+                return recipe.featured
+        }
+            tabSelectionIndex = index ?? 0
         }
 }
 struct RecipeFeaturedView_Previews: PreviewProvider {
--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -22,7 +22,8 @@ struct RecipeListView: View {
                 Text("All Recipes")
                     .bold()
                     .padding(.top, 40)
-                    .font(.largeTitle)
+                    .font(Font.custom("Avenir Heavy", size: 24))
+                
                 
                 ScrollView {
                     LazyVStack (alignment: .leading) {
@@ -40,8 +41,13 @@ struct RecipeListView: View {
                                             .frame(width: 50, height: 50, alignment: .center)
                                             .clipped()
                                             .cornerRadius(5)
+                                        VStack(alignment: .leading) {
                                         Text(r.name)
                                             .foregroundColor(.black)
+                                            .font(Font.custom("Avenir Heavy", size: 16))
+                                            RecipeHighlights(highlights: r.highlights)
+                                                .foregroundColor(.black)
+                                        }
                                     }
                                     
                                 })
